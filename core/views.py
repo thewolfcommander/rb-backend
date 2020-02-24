19220806eded47e0e6e182f63c288adf381bc38a
Manoj Tyagi <tyagimanojtyagi.22@gmail.com>
2020-02-24 21:27:11 +0530

    Contact API Added

--- core/views.py
+++ core/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
 
 from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 
+from core.models import (
+    Contact,
+)
 from core.api.serializers import (
     ContactSerializer,
 )
@@ -10,6 +15,11 @@ from core.api.serializers import (
 class ContactCreateAPIView(APIView):
     authentication_classes = []
     permission_classes = []
+    def get(self, request, format=None):
+        qs = Contact.objects.all()
+        serializer = ContactSerializer(qs, many=True)
+        return Response(serializer.data)
+
     def post(self, request, format=None):
         serializer = ContactSerializer(data=request.data)
         if serializer.is_valid():
